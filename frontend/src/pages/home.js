d03090c4f2f1dc167bb836da0efa1dfde97da0c2
kuntoajin <kuntoaji.engineer@gmail.com>
2023-08-27 01:27:58 +0700

    push

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,36 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container mx-auto'>
+      <header>
+        <div className='flex justify-between'>
+          <div>
+            PDFcenter.
+          </div>
+          <div className='flex'>
+            <ul className='flex'>
+              <li>
+                <a href='/prices'>Prices</a>
+              </li>
+              <li>
+                <a href='/services'>Services</a>
+              </li>
+              <li>
+                <a href='/about'>About</a>
+              </li>
+            </ul>
+            <button>Sign in</button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div>
+          Manage your document
+        </div>
+        <div>
+          <img src='' />
+        </div>
+      </main>
+    </div>
   )
 }
 
