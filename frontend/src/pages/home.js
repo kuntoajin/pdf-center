4d6a245595ebe8ad7554a4cea1e48478e706a029
kuntoajin <kuntoaji.engineer@gmail.com>
2023-08-27 12:32:05 +0700

    push

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,35 +1,39 @@
 const Home = () => {
   return (
-    <div className='container mx-auto'>
-      <header>
-        <div className='flex justify-between'>
-          <div>
-            PDFcenter.
+    <div className='bg-gradient-to-r from-pink-500 to-yellow-500 h-screen'>
+      <div className='container mx-auto flex flex-col text-white h-full'>
+        <header>
+          <div className='flex justify-between'>
+            <div className='font-bold text-[40px]'>
+              PDFcenter.
+            </div>
+            <div className='flex'>
+              <ul className='flex'>
+                <li>
+                  <a href='/prices'>Prices</a>
+                </li>
+                <li>
+                  <a href='/services'>Services</a>
+                </li>
+                <li>
+                  <a href='/about'>About</a>
+                </li>
+              </ul>
+              <button>Sign in</button>
+            </div>
           </div>
-          <div className='flex'>
-            <ul className='flex'>
-              <li>
-                <a href='/prices'>Prices</a>
-              </li>
-              <li>
-                <a href='/services'>Services</a>
-              </li>
-              <li>
-                <a href='/about'>About</a>
-              </li>
-            </ul>
-            <button>Sign in</button>
+        </header>
+        <main className='flex flex-auto'>
+          <div className='flex justify-between font-bold text-[60px] h-full items-center'>
+            <div>
+              Manage your document in simple way.
+            </div>
+            <div>
+              <img src='/hero.svg' />
+            </div>
           </div>
-        </div>
-      </header>
-      <main>
-        <div>
-          Manage your document
-        </div>
-        <div>
-          <img src='' />
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   )
 }
